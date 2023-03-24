eac34f22b262e612d82b3f6ac0c5b25d3480fa8c
Mancoba Dlamini <mancobasanele@gmail.com>
2023-03-23 22:59:43 -0700

    Thu 23 Mar 2023 10:59:43 PM PDT

--- 0x01-git/my_script.sh
+++ 0x01-git/my_script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-history -w
-history >> /root/alx-pre_course/0x01-git/history.txt
+
+history -w >> /root/alx-pre_course/0x01-git/history1.txt
 
 git add .
 git commit -m "$(date)" 
 git push -f
+
